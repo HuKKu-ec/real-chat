4dcef7713f94b1f6dfed4c0172b7e4a72138b7c8
unknown <hukbathelachola2311@gmail.com>
2022-11-08 20:57:36 +0530

    some final touch

--- src/Components/ChatMenu.js
+++ src/Components/ChatMenu.js
@@ -1,72 +1,49 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { signOut } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import { auth, db } from '../Firebase/config';
-import { doc, getDoc } from 'firebase/firestore';
-
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import React, { useContext } from 'react';
+import { db } from '../Firebase/config';
 import { AuthContext } from '../Context/AuthContext';
+import { Scrollbars } from 'react-custom-scrollbars-2';
 
-const ChatHeader = () => {
-  const { currentUser } = useContext(AuthContext);
-  const [userName, setUserName] = useState('');
+const ChatMenu = ({ data }) => {
+  const { currentUser, setReciever } = useContext(AuthContext);
 
-  const navigate = useNavigate();
-  const profileHandler = () => {
-    navigate('/profile');
-  };
-  //logout button firestore function
-  const logoutHandler = () => {
-    signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-        navigate('/signup');
-      })
-      .catch((err) => {
-        // An error happened.
+  //fethching chats of users from firestore
+  const clickPersonHandler = async (reciever) => {
+    setReciever(reciever);
+    const combineId =
+      reciever.uid > currentUser.uid
+        ? reciever.uid + currentUser.uid
+        : currentUser.uid + reciever.uid;
+    const res = await getDoc(doc(db, 'chats', combineId));
+    if (!res.exists()) {
+      await setDoc(doc(db, 'chats', combineId), {
+        messages: [],
+      }).catch((err) => {
         alert(err.message);
-        navigate('/chat');
       });
+    }
   };
-  useEffect(() => {
-    //fetch users data from firestore
-    const fetchUserDetails = async () => {
-      const docSnap = await getDoc(doc(db, 'users', currentUser.uid));
-      if (docSnap.exists()) {
-        const name = docSnap.data().name;
-        setUserName(name);
-      }
-    };
-    fetchUserDetails();
-  }, [currentUser.uid]);
-
   return (
-    <div className="chat-header-container">
-      <h1 className="heading">Real Chat</h1>
-
-      <button className="logout" onClick={logoutHandler}>
-        Logout
-      </button>
-      {userName && userName !== 'undefined' ? (
-        <p className="profile" onClick={profileHandler}>
-          <br></br>change profile
-        </p>
-      ) : (
-        ''
-      )}
-      <p className="profile">
-        {'          '}
-        {currentUser ? currentUser.phoneNumber : ''}
-      </p>
-
-      {userName === '' || userName === 'undefined' ? (
-        <p className="profile" onClick={profileHandler}>
-          setup your profile
-        </p>
-      ) : (
-        <p className="profile">{userName}</p>
-      )}
+    <div className="chat-menu-container">
+      <Scrollbars autoHide style={{ height: '78vh' }}>
+        {data ? (
+          data.map((value, i) => {
+            return (
+              <div
+                key={i}
+                className="box"
+                onClick={() => clickPersonHandler(value)}
+              >
+                {value.name}
+              </div>
+            );
+          })
+        ) : (
+          <div></div>
+        )}
+      </Scrollbars>
     </div>
   );
 };
 
-export default ChatHeader;
+export default ChatMenu;
